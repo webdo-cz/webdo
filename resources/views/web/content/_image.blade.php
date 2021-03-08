b235a8fa262cde1221e653169632b9daa60ba2bd
Ondřej Štěpán <o.stepan@outlook.com>
2021-03-08 20:21:36 +0100

    fix upload image button design

--- resources/views/web/content/_image.blade.php
+++ resources/views/web/content/_image.blade.php
@@ -16,15 +16,15 @@
             <button
                 wire:click="openModal({{ $item }})"
                 type="button"
-                class="z-10 -mr-5 bg-white shadow cursor-pointer btn text-light-blue-500 hover:text-light-blue-800"
+                class="z-10 bg-white border btn text-light-blue-500 hover:text-light-blue-800"
             >
-                Vybrat obrázek
+                Nahrát obrázek
             </button>
             @if(isset($state[$item]['upload'][$lang]) && is_object($state[$item]['upload'][$lang]))
                 <button 
                     wire:click="$set('state.{{ $item }}.upload.{{ $lang }}', null)" 
                     type="button" 
-                    class="py-2 pl-4 pr-3 text-white rounded-l-none rounded-xl bg-light-blue-400 hover:bg-light-blue-500"
+                    class="py-2 pl-4 pr-3 -ml-5 text-white rounded-l-none rounded-xl bg-light-blue-400 hover:bg-light-blue-500"
                 >
                     <svg class="w-4 h-5 ml-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6"></path>
@@ -34,7 +34,7 @@
                 <button
                     wire:click="$set('state.{{ $item }}.delete.{{ $lang }}', true)"
                     type="button"
-                    class="py-2 pl-4 pr-3 text-white bg-red-400 rounded-l-none rounded-xl hover:bg-red-500"
+                    class="py-2 pl-4 pr-3 -ml-5 text-white bg-red-400 rounded-l-none rounded-xl hover:bg-red-500"
                 >
                     <svg class="w-4 h-5 ml-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
@@ -44,7 +44,7 @@
                 <button 
                     wire:click="$set('state.{{ $item }}.delete.{{ $lang }}', false)" 
                     type="button" 
-                    class="py-2 pl-4 pr-3 text-white rounded-l-none rounded-xl bg-light-blue-400 hover:bg-light-blue-500"
+                    class="py-2 pl-4 pr-3 -ml-5 text-white rounded-l-none rounded-xl bg-light-blue-400 hover:bg-light-blue-500"
                 >
                     <svg class="w-4 h-5 ml-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6"></path>
