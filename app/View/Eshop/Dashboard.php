73cb42d30e7cc04d3790f6b442c67222cb975261
Ondřej Štěpán <o.stepan@outlook.com>
2021-06-14 21:22:14 +0200

    Update Dashboard.php

--- app/View/Eshop/Dashboard.php
+++ app/View/Eshop/Dashboard.php
@@ -2,7 +2,9 @@
 
 namespace App\View\Eshop;
 
+use App\Models\Post;
 use App\Models\Eshop\Order;
+use App\Models\Eshop\OrderItem;
 use Carbon\Carbon;
 use Livewire\Component;
 
@@ -13,11 +15,59 @@ class Dashboard extends Component
     public $ordersMonth;
     public $valueWeek;
 
+    public $counts, $countsSend;
+    public $products, $warehouse, $totals, $totalsMonth;
+
     public function mount() {
         $this->ordersDay = Order::where('cart', '0')->whereDate('updated_at', Carbon::today())->count();
         $this->ordersWeek = Order::where('cart', '0')->whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->count();
         $this->ordersMonth = Order::where('cart', '0')->whereMonth('updated_at', Carbon::now()->month)->count();
         $this->valueWeek = Order::where('cart', '0')->whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->avg('total');
+    
+        $this->products = Post::where('type', 'product')->get();
+
+        $this->totals = Order::where('cart', '0')
+        ->whereNotIn('status',['canceled', 'waiting-for-payment'])
+        ->where('submited_at', '>=', \Carbon\Carbon::today()->subMonth())
+        ->groupBy('date')
+        ->orderBy('date', 'DESC')
+        ->get([
+            \DB::raw('Date(submited_at) as date'),
+            \DB::raw('SUM(total) as "total"')
+        ])->keyBy('date')->toArray();
+
+        $this->totalsMonth = Order::where('cart', '0')
+        ->whereNotIn('status',['canceled', 'waiting-for-payment'])
+        ->where( \DB::raw('YEAR(submited_at)'), '=', \Carbon\Carbon::now('Y') )
+        ->selectRaw('year(created_at) year, month(created_at) month, SUM(total) as "total"')
+        ->groupBy('year', 'month')
+        ->orderBy('year', 'desc')
+        ->get()->keyBy('month')->toArray();
+
+        $ordersSend = Order::where('cart', '0')->whereIn('status',['send', 'delivered', 'done'])->pluck('id');
+        $orders = Order::where('cart', '0')->whereNotIn('status',['canceled', 'waiting-for-payment'])->pluck('id');
+
+        $this->countsSend = \DB::table('eshop_order_items')->whereIn('order_id', $ordersSend)
+        ->select('variant_id',\DB::raw('SUM(quantity) as total_quantity'))
+        ->groupBy('variant_id')
+        ->get()->keyBy('variant_id')
+        ->toArray();
+        $this->counts = \DB::table('eshop_order_items')->whereIn('order_id', $orders)
+        ->select('variant_id',\DB::raw('SUM(quantity) as total_quantity'))
+        ->groupBy('variant_id')
+        ->get()->keyBy('variant_id')
+        ->toArray();
+
+        $variants = OrderItem::whereIn('order_id', $ordersSend)->orderBy('name')->get()->unique('variant_id')->keyBy('name');
+        
+
+        $this->warehouse = collect($variants)->map(function ($variant, $name) {
+            return [
+                'name' => $name,
+                'all' => $this->counts[$variant->variant_id]->total_quantity,
+                'send' => $this->countsSend[$variant->variant_id]->total_quantity
+            ];
+        });
     }
 
     public function render()
