b63637b6b70da639b0cba1def719a61c33d3879b
Ondřej Štěpán <o.stepan@outlook.com>
2021-06-14 21:22:18 +0200

    Update dashboard.blade.php

--- resources/views/eshop/dashboard.blade.php
+++ resources/views/eshop/dashboard.blade.php
@@ -2,7 +2,7 @@
     @section('title')
         {{ __('eshop/dashboard.title') }}
     @endsection
-    <div class="flex space-x-4">
+    <div class="flex mb-4 space-x-4">
         <div class="flex w-1/4 p-4 bg-white">
             <div class="relative text-light-blue-200">
                 <svg class="w-12 h-12" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -57,5 +57,116 @@
             </div>
         </div>
     </div>
+    <div class="flex space-x-4">
+        <div class="w-1/2">
+            <table class="w-full mb-4 divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" colspan="2" class="px-6 py-4 text-xs font-medium tracking-wider text-left text-gray-400 uppercase">
+                            Obrat ve dnech
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="w-full bg-white divide-y divide-gray-100">
+                    @foreach ($totals as $key => $item)
+                        <tr>
+                            <td class="w-full px-6 py-3 whitespace-nowrap">
+                                {{ date("d.m.Y", strtotime($key)) }}
+                            </td>
+                            <td class="w-full px-6 py-3 text-right whitespace-nowrap">
+                                {{ $item['total'] }} Kč
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="w-1/2">
+            <table class="w-full mb-4 divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" colspan="2" class="px-6 py-4 text-xs font-medium tracking-wider text-left text-gray-400 uppercase">
+                            Obrat v měsících
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="w-full bg-white divide-y divide-gray-100">
+                    @foreach ($totalsMonth as $key => $item)
+                        <tr>
+                            <td class="w-full px-6 py-3 whitespace-nowrap">
+                                {{ $key }}.{{ $item['year'] }}
+                            </td>
+                            <td class="w-full px-6 py-3 text-right whitespace-nowrap">
+                                {{ $item['total'] }} Kč
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <table class="min-w-full mb-4 divide-y divide-gray-200">
+        <thead class="bg-gray-50 whitespace-nowrap">
+            <tr>
+                <th scope="col" class="px-6 py-4 text-xs font-medium tracking-wider text-left text-gray-400 uppercase">
+                    Sklad
+                </th>
+                <th scope="col" class="px-6 py-4 text-xs font-medium tracking-wider text-right text-gray-400 uppercase">
+                    S (25)
+                </th>
+                <th scope="col" class="px-6 py-4 text-xs font-medium tracking-wider text-right text-gray-400 uppercase">
+                    M (40)
+                </th>
+                <th scope="col" class="px-6 py-4 text-xs font-medium tracking-wider text-right text-gray-400 uppercase">
+                    L (40)
+                </th>
+                <th scope="col" class="px-6 py-4 text-xs font-medium tracking-wider text-right text-gray-400 uppercase">
+                    XL (40/50)
+                </th>
+                <th scope="col" class="px-6 py-4 text-xs font-medium tracking-wider text-right text-gray-400 uppercase">
+                    2XL (15/35)
+                </th>
+                <th scope="col" class="px-6 py-4 text-xs font-medium tracking-wider text-right text-gray-400 uppercase">
+                    3XL (10/30)
+                </th>
+            </tr>
+        </thead>
+        <tbody class="w-full bg-white divide-y divide-gray-100">
+            @foreach ($products  as $key => $product)
+                @if(!empty(collect($warehouse)->where('name', $product->title . ' - M')->first()))
+                    <tr>
+                        <td class="w-full px-6 py-4 whitespace-nowrap">
+                            {{ $product->title }}
+                        </td>
+                        <td class="w-full px-6 py-4 text-right whitespace-nowrap">
+                            {{ collect($warehouse)->where('name', $product->title . ' - S')->first()['send'] ?? '0' }}
+                            ({{ collect($warehouse)->where('name', $product->title . ' - S')->first()['all'] ?? '0' }})
+                        </td>
+                        <td class="w-full px-6 py-4 text-right whitespace-nowrap">
+                            {{ collect($warehouse)->where('name', $product->title . ' - M')->first()['send'] ?? '0' }}
+                            ({{ collect($warehouse)->where('name', $product->title . ' - M')->first()['all'] ?? '0' }})
+                        </td>
+                        <td class="w-full px-6 py-4 text-right whitespace-nowrap">
+                            {{ collect($warehouse)->where('name', $product->title . ' - L')->first()['send'] ?? '0' }}
+                            ({{ collect($warehouse)->where('name', $product->title . ' - L')->first()['all'] ?? '0' }})
+                        </td>
+                        <td class="w-full px-6 py-4 text-right whitespace-nowrap">
+                            {{ collect($warehouse)->where('name', $product->title . ' - XL')->first()['send'] ?? '0' }}
+                            ({{ collect($warehouse)->where('name', $product->title . ' - XL')->first()['all'] ?? '0' }})
+                        </td>
+                        <td class="w-full px-6 py-4 text-right whitespace-nowrap">
+                            {{ collect($warehouse)->where('name', $product->title . ' - 2XL')->first()['send'] ?? '0' }}
+                            ({{ collect($warehouse)->where('name', $product->title . ' - 2XL')->first()['all'] ?? '0' }})
+                        </td>
+                        <td class="w-full px-6 py-4 text-right whitespace-nowrap">
+                            {{ collect($warehouse)->where('name', $product->title . ' - 3XL')->first()['send'] ?? '0' }}
+                            ({{ collect($warehouse)->where('name', $product->title . ' - 3XL')->first()['all'] ?? '0' }})
+                        </td>
+                    </tr>
+                @endif
+            @endforeach
+        </tbody>
+    </table>
     
 </div>
+
