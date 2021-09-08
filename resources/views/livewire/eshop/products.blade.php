88645a782a18e430fb45974b45be3a00130bb7aa
Ondřej Štěpán <o.stepan@outlook.com>
2021-09-08 09:27:02 +0200

    remove old products table

--- resources/views/livewire/eshop/products.blade.php
+++ resources/views/livewire/eshop/products.blade.php
@@ -78,26 +78,4 @@
             {{ $products->links() }}
         </div>
     </div>
-
-
-    <div class="space-y-2">
-        @foreach($products as $key => $product)
-            <div class="flex flex-col justify-between flex-grow w-full px-4 py-4 text-sm font-medium leading-5 bg-white md:items-center md:flex-row sm:px-6">
-                <div class="pl-2">
-                    {{ $product->title }}
-                </div>
-                <div class="flex flex-col flex-shrink-0 sm:ml-2 sm:flex-row sm:items-center sm:space-x-4">
-                    <a 
-                        href="{{ url('eshop/product/edit/' . $product->id) }}"
-                        class="px-5 py-2 mr-2 font-semibold text-gray-400 transition duration-200 border rounded-xl hover:text-gray-500"
-                    >
-                        <span>Upravit</span>
-                        <svg class="inline-block w-4 h-4 ml-2 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path>
-                        </svg>
-                    </a>
-                </div>
-            </div>
-        @endforeach
-    </div>
 </div>
